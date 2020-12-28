8c4856eb79eabfe09401baf1b2254e4b35133c35
Bengt Uggla <bengt.uggla@gmail.com>
2020-12-28 12:55:14 +0100

    Enabled scroll link JS to close menu when internal linking

--- js/script.js
+++ js/script.js
@@ -7,3 +7,28 @@ const links = document.getElementById("nav-links");
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+
+
+// ********** smooth scroll ************
+// REables that the menu to close when going from menu links to internal page links. .scroll-link class has to be on every target a link
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
