ef9eec5d7f133bbb9ed4cb31b93da70f526e3dc6
Bengt Uggla <bengt.uggla@gmail.com>
2020-12-28 12:43:32 +0100

    Nav-links default height 0px show-links class toggle nav-links on off 280px with JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+// Toggle nav-bar on/off
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
