79b64f2e525dab02ce5ba7833a811fbb93f8c6b4
Bengt Uggla <bengt.uggla@gmail.com>
2021-01-12 18:07:28 +0100

    trying to fix resonsive size problems phone images showcase. Changed classList togglebtn style none outline when clicked

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-
-
 // Toggle nav-bar on/off
-const navBtn = document.getElementById("nav-toggle");
-const links = document.getElementById("nav-links");
+const navBtn = document.getElementById('nav-toggle');
+const links = document.getElementById('nav-links');
 // add event listener
-navBtn.addEventListener("click", () => {
-  links.classList.toggle("show-links");
+navBtn.addEventListener('click', () => {
+  navBtn.style.outline = 'none';
+  links.classList.toggle('show-links');
 });
-
 
 // ********** smooth scroll ************
 // REables that the menu to close when going from menu links to internal page links. .scroll-link class has to be on every target a link
 // select links
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
     // prevent default
     e.preventDefault();
-    links.classList.remove("show-links");
+    links.classList.remove('show-links');
 
-    const id = e.target.getAttribute("href").slice(1);
+    const id = e.target.getAttribute('href').slice(1);
     const element = document.getElementById(id);
     //
     let position = element.offsetTop - 62;
@@ -28,9 +26,7 @@ scrollLinks.forEach(link => {
       left: 0,
       // top: element.offsetTop,
       top: position,
-      behavior: "smooth"
+      behavior: 'smooth',
     });
   });
 });
-
-
